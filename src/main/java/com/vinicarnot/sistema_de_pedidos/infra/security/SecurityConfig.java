7b090a91dcc0e487f8516c43a9e5cda7fe6db78b
Vinicius Carnot <viniciusm.carnot@gmail.com>
2025-12-24 13:37:11 -0300

    feat (SecurityConfig, TokenService): criação das classes.

--- src/main/java/com/vinicarnot/sistema_de_pedidos/infra/security/SecurityConfig.java
+++ src/main/java/com/vinicarnot/sistema_de_pedidos/infra/security/SecurityConfig.java
@@ -3,15 +3,25 @@ package com.vinicarnot.sistema_de_pedidos.infra.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final SecurityTokenFilter securityTokenFilter;
+
+    public SecurityConfig(SecurityTokenFilter securityTokenFilter) {
+        this.securityTokenFilter = securityTokenFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -32,6 +42,16 @@ public class SecurityConfig {
                 .addFilterBefore(securityTokenFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
 
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
